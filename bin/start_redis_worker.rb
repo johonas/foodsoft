49a95ff7f37f2716013bdd9ebf8c81b2ee06add1
Jonas Dinkelmann <none>
2018-12-18 12:19:04 +0100

    Start only one redis worker

--- bin/start_redis_worker.rb
+++ bin/start_redis_worker.rb
@@ -1,2 +1,2 @@
 Resque.workers.each {|w| w.unregister_worker}
-exec("RAILS_ENV=production QUEUE=#{FoodsoftConfig[:redis_queue]} COUNT=5 PIDFILE=./resque.pid BACKGROUND=yes bundle exec rake resque:workers")
+exec("RAILS_ENV=production QUEUE=#{FoodsoftConfig[:redis_queue]} PIDFILE=./resque.pid BACKGROUND=yes bundle exec rake resque:work")
