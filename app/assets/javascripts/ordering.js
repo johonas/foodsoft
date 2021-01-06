5a93eaf8ecd937cb04f47c9a7e987c6356399734
Jonas Dinkelmann <jonas@dinkelmann.ch>
2021-01-06 16:56:24 +0100

    Remove tolerance and min quantity on client side

--- app/assets/javascripts/ordering.js
+++ app/assets/javascripts/ordering.js
@@ -48,125 +48,53 @@ function addData(orderArticleId, itemPrice, itemUnit, itemSubtotal, itemQuantity
 }
 
 function increaseQuantity(item) {
-    var $el   = $('#q_' + item),
-        value = Number($el.val()) + 1,
-        max   = $el.data('max');
-    if (value > max) { value = max; }
-    if (!isStockit || (value <= (quantityAvailable[item] + itemsAllocated[item]))) {
-        update(item, value, $('#t_' + item).val());
-    }
+    var $el   = $('#q_' + item);
+    var quantity = Number($el.val()) + 1;
+
+    update(item, quantity);
 }
 
 function decreaseQuantity(item) {
-    var $el   = $('#q_' + item),
-        value = Number($el.val()) - 1,
-        min   = $el.data('min') || 0;
-    if (value >= min) {
-        update(item, value, $('#t_' + item).val());
+    var $el   = $('#q_' + item);
+    var quantity = Number($el.val()) - 1;
+
+    if (quantity >= 0) {
+        update(item, quantity);
     }
 }
 
-function increaseTolerance(item) {
-    var $el   = $('#t_' + item),
-        value = Number($el.val()) + 1;
-        max   = $el.data('max');
-    if (value > max) { value = max; }
-    update(item, $('#q_' + item).val(), value);
-}
+function updateManually(item, quantity) {
+    if ($.isNumeric(quantity) && parseInt(quantity) < 0) {
+        quantity = 0;
+    }
 
-function decreaseTolerance(item) {
-    var $el   = $('#t_' + item),
-        value = Number($el.val()) - 1,
-        min   = $el.data('min') || 0;
-    if (value >= min) {
-        update(item, $('#q_' + item).val(), value);
+    if (quantity >= 0) {
+        update(item, quantity);
     }
 }
 
-function update(item, quantity, tolerance) {
+function update(item, quantity) {
     // set modification flag
-    modified = true
+    modified = true;
 
-    // update hidden input fields
     $('#q_' + item).val(quantity);
-    $('#t_' + item).val(tolerance);
 
     // calculate how many units would be ordered in total
-    var units = calcUnits(unit[item], quantityOthers[item] + Number(quantity), toleranceOthers[item] + Number(tolerance));
-    if (unitCompletedFromTolerance(unit[item], quantityOthers[item] + Number(quantity), toleranceOthers[item] + Number(tolerance))) {
-        $('#units_' + item).html('<span style=\"color:grey\">' + String(units) + '</span>');
-    } else {
-        $('#units_' + item).html(String(units));
-    }
+    var units = calcUnits(quantityOthers[item] + Number(quantity), unit[item]);
 
-    // update used/unused quantity
-    var available = Math.max(0, units * unit[item] - quantityOthers[item]);
-    var q_used = Math.min(available, quantity);
-    // ensure that at least the amout of items this group has already been allocated is used
-    if (quantity >= itemsAllocated[item] && q_used < itemsAllocated[item]) {
-        q_used = itemsAllocated[item];
-    }
-    $('#q_used_' + item).html(String(q_used));
-    $('#q_unused_' + item).html(String(quantity - q_used));
-    $('#q_total_' + item).html(String(Number(quantity) + quantityOthers[item]));
-
-    // update used/unused tolerance
-    if (unit[item] > 1) {
-        available = Math.max(0, available - q_used - toleranceOthers[item]);
-        t_used = Math.min(available, tolerance);
-        $('#t_used_' + item).html(String(t_used));
-        $('#t_unused_' + item).html(String(tolerance - t_used));
-        $('#t_total_' + item).html(String(Number(tolerance) + toleranceOthers[item]));
-    }
+    $('#units_' + item).html(units);
+    $('#q_total_' + item).html(Number(quantity) + quantityOthers[item]);
 
     // update total price
-    if(toleranceIsCostly == true) {
-        itemTotal[item] = price[item] * (Number(quantity) + Number(tolerance));
-    } else {
-        itemTotal[item] = price[item] * (Number(quantity));
-    }
+    itemTotal[item] = price[item] * Number(quantity);
     $('#price_' + item + '_display').html(I18n.l("currency", itemTotal[item]));
 
-    // update missing units
-    var missing_units = calcMissingItems(unit[item], quantityOthers[item] + Number(quantity), toleranceOthers[item] + Number(tolerance)),
-        missing_units_css = '';
-
-    if (missing_units <= 0 || missing_units == unit[item]) {
-        missing_units = 0;
-        if (units > 0) {
-            missing_units_css = 'missing-none';
-        } else {
-            missing_units_css = '';
-        }
-    } else if (missing_units == 1) {
-        missing_units_css = 'missing-few';
-    } else {
-        missing_units_css = 'missing-many';
-    }
-    $('#missing_units_' + item)
-        .html(String(missing_units))
-        .closest('tr')
-        .removeClass('missing-many missing-few missing-none')
-        .addClass(missing_units_css);
-
     updateBalance();
-    updateButtons($('#q_'+item).closest('tr'));
+    updateButtons($('#q_' + item).closest('tr'));
 }
 
-function calcUnits(unitSize, quantity, tolerance) {
-    var units = Math.floor(quantity / unitSize)
-    var remainder = quantity % unitSize
-    return units + ((remainder > 0) && (remainder + tolerance >= unitSize) ? 1 : 0)
-}
-
-function calcMissingItems(unitSize, quantity, tolerance) {
-    var remainder = quantity % unitSize
-    return remainder > 0 && remainder + tolerance < unitSize ? unitSize - remainder - tolerance : 0
-}
-
-function unitCompletedFromTolerance(unitSize, quantity, tolerance) {
-    var remainder = quantity % unitSize
-    return (remainder > 0 && (remainder + tolerance >= unitSize));
+function calcUnits(quantity, unitSize) {
+    return Math.ceil(quantity / unitSize);
 }
 
 function updateBalance() {
@@ -194,37 +122,24 @@ function updateBalance() {
 }
 
 function updateButtons($el) {
-    // enable/disable buttons depending on min/max vs. value
-    $el.find('a[data-increase_quantity]').each(function() {
-      var $q = $el.find('#q_'+$(this).data('increase_quantity'));
-      $(this).toggleClass('disabled', $q.val() >= $q.data('max'));
-    });
-    $el.find('a[data-decrease_quantity]').each(function() {
-      var $q = $el.find('#q_'+$(this).data('decrease_quantity'));
-      $(this).toggleClass('disabled', $q.val() <= ($q.data('min')||0));
-    });
-    $el.find('a[data-increase_tolerance]').each(function() {
-      var $t = $el.find('#t_'+$(this).data('increase_tolerance'));
-      $(this).toggleClass('disabled', $t.val() >= $t.data('max'));
-    });
-    $el.find('a[data-decrease_tolerance]').each(function() {
-      var $t = $el.find('#t_'+$(this).data('decrease_tolerance'));
-      $(this).toggleClass('disabled', $t.val() <= ($t.data('min')||0));
+    // enable/disable buttons
+    $el.find('a.decrease-quantity').each(function() {
+      var $q = $el.find('#q_' + $(this).data('order-article-id'));
+      $(this).toggleClass('disabled', $q.val() <= 0);
     });
 }
 
 $(function() {
-    $('a[data-increase_quantity]').on('touchclick', function() {
-        increaseQuantity($(this).data('increase_quantity'));
+    $('input.quantity').on('keyup change', function() {
+        updateManually($(this).data('order-article-id'), $(this).val());
     });
-    $('a[data-decrease_quantity]').on('touchclick', function() {
-        decreaseQuantity($(this).data('decrease_quantity'));
+
+    $('a.increase-quantity').on('touchclick', function() {
+        increaseQuantity($(this).data('order-article-id'));
     });
-    $('a[data-increase_tolerance]').on('touchclick', function() {
-        increaseTolerance($(this).data('increase_tolerance'));
-    });
-    $('a[data-decrease_tolerance]').on('touchclick', function() {
-        decreaseTolerance($(this).data('decrease_tolerance'));
+
+    $('a.decrease-quantity').on('touchclick', function() {
+        decreaseQuantity($(this).data('order-article-id'));
     });
 
     $('a[data-confirm_switch_order]').on('touchclick', function() {
